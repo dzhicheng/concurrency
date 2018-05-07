0fc718021fde2da48c9b54cc5a2a6cdc57be27cf
zhicheng <704630320@qq.com>
2018-05-07 16:19:31 +0800

    add annoations

---
+++ src/main/java/com/dongzhic/annoations/NotRecommend.java
@@ -0,0 +1,18 @@
+package com.dongzhic.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author dongzhic
+ * @date 2018.05.07
+ * @desc 用来标记【不推荐】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}
