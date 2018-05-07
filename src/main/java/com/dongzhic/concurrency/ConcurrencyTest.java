84262b6c38221d361253965918bc87ddefeac298
zhicheng <704630320@qq.com>
2018-05-07 18:37:50 +0800

    并发模拟

---
+++ src/main/java/com/dongzhic/concurrency/ConcurrencyTest.java
@@ -0,0 +1,8 @@
+package com.dongzhic.concurrency;
+
+/**
+ * @author dongzhi
+ * @date 2018.05.07
+ */
+public class ConcurrencyTest {
+}
---
+++ src/main/java/com/dongzhic/concurrency/TestController.java
@@ -0,0 +1,21 @@
+package com.dongzhic.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author dongzc
+ * spring boot只会扫描启动类当前包和以下的包
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test () {
+        return "test";
+    }
+}
